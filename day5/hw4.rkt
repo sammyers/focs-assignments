95c06e6db26b21e9e5dc9e89801671b05589e63a
Sam Myers <sam@students.olin.edu>
2016-09-26 01:36:07 -0400

    Turning in my assignment for day 7

--- day5/hw4.rkt
+++ day5/hw4.rkt
@@ -6,3 +6,96 @@
 ;;; [ ] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
+
+(define (run-repl)
+  (display "welcome to my repl.  type some scheme-ish")
+  (repl))
+
+(define (repl)
+  (display "> ")
+  (display (myeval (read)))
+  (newline)
+  (repl))
+
+(define (myeval sexpr)
+  sexpr)
+
+(define operator-list
+  (list (list 'ADD +)
+        (list 'SUB -)
+        (list 'MUL *)
+        (list 'DIV /)
+        (list 'GT >)
+        (list 'LT <)
+        (list 'GE >=)
+        (list 'LE <=)
+        (list 'EQ =)
+        (list 'NEQ (lambda (x y) (not (= x y))))
+        (list 'ANND (lambda (x y) (and x y)))
+        (list 'ORR (lambda (x y) (or x y)))
+        (list 'NOTT not)))
+
+(define (has-key key)
+  (lambda (lst) (eq? (first lst) key))
+)
+
+(define (assq key lst)
+  (let ([match (filter (has-key key) lst)])
+    (if (empty? match)
+        #f
+        (first match))
+  )
+)
+
+(define (basic-calculate operator operand1 operand2)
+  (cond [(eq? operator 'ADD)
+         (+ operand1 operand2)]
+        [(eq? operator 'SUB)
+         (- operand1 operand2)]
+        [(eq? operator 'MUL)
+         (* operand1 operand2)]
+        [(eq? operator 'DIV)
+         (/ operand1 operand2)]
+        [(eq? operator 'GT)
+         (> operand1 operand2)]
+        [(eq? operator 'LT)
+         (< operand1 operand2)]
+        [(eq? operator 'GE)
+         (>= operand1 operand2)]
+        [(eq? operator 'LE)
+         (<= operand1 operand2)]
+        [(eq? operator 'EQ)
+         (= operand1 operand2)]
+        [(eq? operator 'NEQ)
+         (not (= operand1 operand2))]
+        [(eq? operator 'ANND)
+         (and operand1 operand2)]
+        [(eq? operator 'ORR)
+         (or operand1 operand2)]
+  )
+)
+
+(define (evaluate x lookup-list)
+  (cond [(null? x)
+         x]
+        [(number? x)
+         x]
+        [(symbol? x)
+         (second (assq x lookup-list))]
+        [else
+         (cond [(= (length x) 3)
+                (basic-calculate (first x) (evaluate (second x) lookup-list) (evaluate (third x) lookup-list))]       
+               [(= (length x) 2)
+                (not (evaluate (second x) lookup-list))]
+               [(= (length x) 4)
+                (let ([conditional (second x)]
+                      [true-expr (third x)]
+                      [false-expr (fourth x)])
+                  (if (evaluate conditional lookup-list)
+                      (evaluate true-expr lookup-list)
+                      (evaluate false-expr lookup-list)
+                  )
+                )]
+         )]
+  )
+)
--- day7/hw7.md
+++ day7/hw7.md
@@ -6,6 +6,10 @@ Convert (a), and at least one of (b) and (c), to a regular expression.
 
 ![Alt text](fsa-to-re.png)
 
+(a) `01(11)*0`
+(b) `0*10*`
+(c) `1*`
+
 ## 2. Parenthesis Matching
 
 **a**. Construct a regular expression that matches strings where each `(` is followed immediately by a `)`.
@@ -14,11 +18,17 @@ Positive examples: `()`, `()()()`
 
 Negative examples: `(()`, `(())`
 
+**Assume a = `(`, b = `)`**
+
+`(ab)*`
+
 **b**. Construct a regular expression that matches strings where the parentheses match, with depth <= 2.
 
 Positive example: `()(()())()`
 
 Negative examples: `((()))`, `((())())` have three levels of neseting.
+
+`(a(ab)*b)*`
 
 **c**. Construct a regular expression that accepts strings where the parentheses match, with depth <= 3.
 
@@ -26,13 +36,19 @@ Positive examples: `()(()())()`, `(()(()()))()`
 
 Negative example: `(((())))` has four levels of nesting.
 
+`(a(a(ab)*b)*b)*`
+
 ## 3. [optional] Challenge Problems
 
 Construct these. Some you may be able to do by just by thinking. Some may be easier if you construct an automaton and then convert it.
 
 - A regular expressions that matches strings with an odd number of 1s.
 
+`0*1(0*10*10*)*`
+
 - A regular expressions that matches strings with an even number of 0s.
+
+`1*(1*01*01*)*`
 
 - A regular expressions that matches strings with an odd number of 1s AND an even number of 0s.
 
@@ -50,4 +66,8 @@ Learn about [character classes](https://regexone.com/lesson/letters_and_digits) 
 
 Which of these are just notational conveniences, like `[abc]` for `(a|b|c)`?
 
+Character classes, quantifiers
+
 Which increase the power of regular expressions?
+
+Backreferences, anchors
